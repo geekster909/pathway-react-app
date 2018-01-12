ea7d3be6c1ab355e9d23eb17c415efa8f92bfa28
Justin Bond <bond.justink@gmail.com>
2018-01-11 21:11:24 -0800

    initial commit

---
+++ src/components/pathway.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Pathway extends Component {
+
+  render() {
+    return (
+      <div>
+        Pathway React App
+      </div>
+    );
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './index.css';
+import Pathway from './components/pathway';
+// import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<Pathway />, document.getElementById('root'));
+// registerServiceWorker();
