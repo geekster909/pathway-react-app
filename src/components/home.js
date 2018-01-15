4bb4fceca8cb6c3a78e11117adbbdf5ec7554322
Justin Bond <bond.justink@gmail.com>
2018-01-14 20:45:30 -0800

    moved database mount/sync

--- src/components/home.js
+++ src/components/home.js
@@ -3,31 +3,20 @@ import React, { Component } from 'react';
 // import pathsList from '../paths-list';
 import SearchBar from './search_bar';
 import SearchResults from './search_results';
-import base from '../base';
 
 export default class home extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			term: '',
-			paths: {},
+			// paths: {},
 		}
-	}
-
-	componentWillMount() {
-		this.ref = base.syncState('/', {
-				context: this,
-				state: 'paths'
-			});
-	}
-
-	componentWillUnmount() {
-		base.removeBinding(this.ref);
 	}
 
 	pathSearch(term) {
 		this.setState({term});
 	}
+	
 	render() {
 		const pathSearch = _.debounce((term) => { this.pathSearch(term) }, 500 );
 		return (
@@ -36,7 +25,7 @@ export default class home extends Component {
 					onSearchTermChange={pathSearch} />
 				<SearchResults 
 					searchTerm={this.state.term}
-					paths={this.state.paths} />
+					paths={this.props.paths} />
 			</div>
 		);
 	}
--- src/components/pathway.js
+++ src/components/pathway.js
@@ -2,19 +2,45 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import Home from './home';
 import NoMatch from './no_match';
+import base from '../base';
 
 export default class Pathway extends Component {
+	constructor() {
+		super();
+		this.state = {
+			paths: {},
+		}
+	}
+
+	componentWillMount() {
+		this.ref = base.syncState('/', {
+				context: this,
+				state: 'paths'
+			});
+	}
+
+	componentWillUnmount() {
+		base.removeBinding(this.ref);
+	}
+
 	render() {
 		return (
 			<Router>
 				<div>
-					<ul>
-						<li><Link to="/">Home</Link></li>
-					</ul>
-					<Switch>
-						<Route path="/" exact component={Home}/>
-						<Route component={NoMatch}/>
-					</Switch>
+					<div className="header">
+						<ul>
+							<li><Link to="/">Home</Link></li>
+						</ul>
+					</div>
+					<div className="main-content">
+						<Switch>
+							<Route 
+								path="/" 
+								exact 
+								render={()=><Home paths={this.state.paths}/>} />
+							<Route component={NoMatch}/>
+						</Switch>
+					</div>
 				</div>
 			</Router>
 		);
