94013a66f06f0d4c0f1e9ec33cb3591a3ec71366
Justin Bond <bond.justink@gmail.com>
2018-01-19 00:28:29 -0800

    started trail detailed page

--- src/components/trail_detailed.js
+++ src/components/trail_detailed.js
@@ -19,8 +19,32 @@ export default class trail_detailed extends Component {
 	}
 	render() {
 		const trailDetails = this.getTrailDetails();
+		// const trailLevel = trailDetails['skill'].toLowerCase();
+		const trailLevel = trailDetails['skill'];
+		console.log(trailDetails);
 		return (
-			<div>This is a Path details page {trailDetails['name']}</div>
+			<div className="trail--detailed">
+				<div className="trail--detailed__image">
+					<img src={trailDetails['image']} alt=""/>
+				</div>
+				<div className="trail--details">
+					<div className="container">
+						<div className="row">
+							<div className="col-lg-offset-2 col-lg-8">
+								<div className="trail--details__title">
+									<h1>{trailDetails['name']}</h1>
+								</div>
+								<div className={`trail--details__level ${trailLevel}`}>
+									{trailLevel}
+								</div>
+								<div className="trail--details__miles">
+									Miles: {trailDetails['miles']}
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		);
 	}
 }
